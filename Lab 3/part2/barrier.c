128d11ce608f871fca4b1f4d73a18c4c13d2ed55
nicktohzyu <34980083+nicktohzyu@users.noreply.github.com>
2022-03-14 18:55:57 +0800

    Update lab3 part 1 and 2

--- Lab 3/part2/barrier.c
+++ Lab 3/part2/barrier.c
@@ -4,28 +4,33 @@
 #include <semaphore.h>
 
 int* count;
+int* numproc;
 sem_t* barrier;
 sem_t* mutex;
-int shmid, shmid2, shmid3;
+int count_shmid, numproc_shmid, barrier_shmid, mutex_shmid;
 
-void init_barrier(int numproc) {
-	shmid = shmget(IPC_PRIVATE, sizeof(int)*2, IPC_CREAT | 0600);
-	shmid2 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
-	shmid3 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
-	count = (int *) shmat(shmid, NULL, 0);
-	barrier = (sem_t *) shmat(shmid2, NULL, 0);
-	mutex = (sem_t *) shmat(shmid3, NULL, 0);
-	count[0] = 0;
-	count[1] = numproc;
+void init_barrier(int _numproc) {
+    count_shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
+    numproc_shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
+    barrier_shmid = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
+    mutex_shmid = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
+
+	count = (int *) shmat(count_shmid, NULL, 0);
+    numproc = (int *) shmat(numproc_shmid, NULL, 0);
+	barrier = (sem_t *) shmat(barrier_shmid, NULL, 0);
+	mutex = (sem_t *) shmat(mutex_shmid, NULL, 0);
+
+	*count = 0;
+	*numproc = _numproc;
 	sem_init(barrier, 1, 0);
 	sem_init(mutex, 1, 1);
 }
 
 void reach_barrier() {
 	sem_wait(mutex);
-	count[0]++;
+    (*count)++;
 	sem_post(mutex);
-	if (count[0] == count[1]) {
+	if (*count == *numproc) {
 		sem_post(barrier);
 	} else {
 		sem_wait(barrier);
@@ -38,9 +43,9 @@ void destroy_barrier(int my_pid) {
     if(my_pid != 0) {
     	sem_destroy(barrier);
     	sem_destroy(mutex);
-    	shmctl(shmid, IPC_RMID, 0);
-    	shmctl(shmid2, IPC_RMID, 0);
-    	shmctl(shmid3, IPC_RMID, 0);
+    	shmctl(count_shmid, IPC_RMID, 0);
+    	shmctl(barrier_shmid, IPC_RMID, 0);
+    	shmctl(mutex_shmid, IPC_RMID, 0);
     }
 }
 
